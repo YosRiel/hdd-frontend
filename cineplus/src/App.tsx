c6e7f11ffdf800b71ad1e05ad5636724da8414b1
yosriel <96359361+YosRiel@users.noreply.github.com>
2025-09-20 00:28:50 -0500

    asd

---
+++ cineplus/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      {/* Puedes personalizar el contenido principal aquí */}
+      <h1>Bienvenido a Cineplus</h1>
+    </div>
+  );
+};
+
+export default App;
